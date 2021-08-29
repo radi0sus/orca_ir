7a0be53ea5b66a77407472700ebee3a13b7db504
Sebastian Dechert <p212121@googlemail.com>
2021-08-29 18:49:59 +0200

    Update orca-ir.py

--- orca-ir.py
+++ orca-ir.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import sys                              #sys files processing
 import os                               #os file processing
 import re                               #regular expresions
@@ -9,17 +8,26 @@ import argparse                         #argument parser
 from scipy.signal import find_peaks     #peak detection
 
 # global constants
-found_ir_section=False              #check for IR data in out
-specstring_start='IR SPECTRUM'      #check orca.out from here
-specstring_end='The first'          #stop reading orca.out from here
-w = 15                              #w = line width, FWHM
-wn_add = 250                        #add +250 to spectra x (required for convolution)
-high_to_low_wn = True               #go from high to low wavenumber, normal for IR spectra, low wn to high wn if False
-transm_style = True                 #show spectra in transmition style, normal for IR spectra, absorption style if False
-show_grid = True                    #show grid
-show_single_gauss = False           #show single gauss functions
-show_single_gauss_area = False      #show single gauss functions - area plot
-label_rel_pos_y = -15               #-15 for transmission style, 5 for absortion style 
+found_ir_section=False                  #check for IR data in out
+specstring_start='IR SPECTRUM'          #check orca.out from here
+specstring_end='The first'              #stop reading orca.out from here
+w = 15                                  #w = line width, FWHM
+wn_add = 250                            #add +250 to spectra x (required for convolution)
+# plot config section
+high_to_low_wn = True                   #go from high to low wavenumber, normal for IR spectra, low wn to high wn if False
+transm_style = True                     #show spectra in transmition style, normal for IR spectra, absorption style if False
+show_grid = True                        #show grid if True
+show_single_gauss = False               #show single gauss functions if True
+show_single_gauss_area = False          #show single gauss functions - area plot if True
+label_rel_pos_y = -15                   #-15 for transmission style, 5 for absortion style 
+save_spectrum = True                    #save spectrum if True
+show_spectrum = True                    #show the matplotlib window if True
+label_peaks = True                      #show peak labels if True
+minor_ticks = True                      #show minor ticks if True
+spectrum_title = "IR spectrum"          #title
+spectrum_title_weight = "bold"          #weight of the title font: 'normal' | 'bold' | 'heavy' | 'light' | 'ultrabold' | 'ultralight'
+y_label = "Intensity"                   #label of the y axis
+x_label = r'$\tilde{\nu}$ /cm$^{-1}$'   #label of the x axis
 
 #global lists
 modelist=list()         #mode
@@ -103,18 +111,21 @@ ax.stem(freqlist,intenslist,"dimgrey",markerfmt=" ",basefmt=" ")
 #ax.plot(peaks,plt_range_gauss_sum_y[peaks],"|")
 
 #label peaks
-if not transm_style:
-    label_rel_pos_y=5   #in case of absorption style spectra
+if label_peaks:
+    if not transm_style:
+        label_rel_pos_y=5   #in case of absorption style spectra
+        
+    for index, txt in enumerate(peaks):
+        ax.annotate(peaks[index],xy=(peaks[index],plt_range_gauss_sum_y[peaks[index]]),ha="center",rotation=90,size=6,
+            xytext=(0,label_rel_pos_y), textcoords='offset points')
     
-for index, txt in enumerate(peaks):
-    ax.annotate(peaks[index],xy=(peaks[index],plt_range_gauss_sum_y[peaks[index]]),ha="center",rotation=90,size=6,
-        xytext=(0,label_rel_pos_y), textcoords='offset points')
-    
-ax.set_xlabel(r'$\tilde{\nu}$ /cm$^{-1}$')    #label x axis
-ax.set_ylabel('Intensity')                      #label y axis
-ax.set_title('IR spectrum')                     #title
-ax.get_yaxis().set_ticks([])                    #remove ticks from y axis
-
+ax.set_xlabel(x_label)                                          #label x axis
+ax.set_ylabel(y_label)                                          #label y axis
+ax.set_title(spectrum_title,fontweight=spectrum_title_weight)   #title
+ax.get_yaxis().set_ticks([])                                    #remove ticks from y axis
+plt.tight_layout()                                              #tight layout
+if minor_ticks:
+    ax.minorticks_on()                                          #show minor ticks
 
 #x,y axis manipulation
 if transm_style:
@@ -138,8 +149,11 @@ params = plt.gcf()
 plSize = params.get_size_inches()
 params.set_size_inches((plSize[0]*N, plSize[1]*N))
 
-filename, file_extension = os.path.splitext(args.filename)
-#plt.savefig(f"{filename}-ir.png", dpi=300)
+#save the plot
+if save_spectrum:
+    filename, file_extension = os.path.splitext(args.filename)
+    plt.savefig(f"{filename}-ir.png", dpi=300)
 
 #show the plot
-plt.show()
+if show_spectrum:
+    plt.show()
