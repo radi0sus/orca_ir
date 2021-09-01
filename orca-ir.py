7c766052e8d16e573bd9369cbd7f31cc2cfe2dd9
Sebastian Dechert <p212121@googlemail.com>
2021-09-01 20:07:26 +0200

    Update orca-ir.py

--- orca-ir.py
+++ orca-ir.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 '''
+# orca-ir
 Python 3 script for (hassle-free) plotting IR spectra from ORCA output files. 
-It combines the stick spectrum with the convoluted spectrum (gaussian line shape). The full spectrum or parts of the spectrum (via matplotlib window) can be plotted.
+It combines the stick spectrum with the convoluted spectrum (gaussian line shape). 
+The full spectrum or parts of the spectrum (via matplotlib window) can be plotted.
 
 ## External Modules
  `re`, `numpy`, `matplotlib`, `scipy`  
@@ -14,7 +16,6 @@ it will save a plot as PNG bitmap:
 
 
 ## Command-line options
-
 - `filename` required: filename
 - `-w` optional: line width of the gaussian (default is 15)
 - `-s` optional: shows the `matplotlib` window
@@ -23,17 +24,21 @@ it will save a plot as PNG bitmap:
 ## Script options
 There are numerous ways to configure the spectrum in the script:
 Check `# plot config section - configure here` in the script. 
-Here, you can configure an absorption or transmittance plot. You can even plot the single gaussian functions.
+Here, you can configure an absorption or transmittance plot. 
+You can even plot the single gaussian functions.
 
 ## Code options
-Colors, line thickness, line styles, level pf peak detection and more can be changed in the code directly.
+Colors, line thickness, line styles, level peak detection and 
+more can be changed in the code directly.
 
 ## Special options and limitations
-The spectrum always starts at zero and ends at the maximum wave number. If you need only a part of the spectrum, you can start the script with:
+The spectrum always starts at zero and ends at the maximum wave number. 
+If you need only a part of the spectrum, you can start the script with:
 `python3 orca-ir.py filename -s`
 and use the matplotlib window to zoom to an area of interest and save it.
 
-The PNG file will be replaced everytime you start the script with the same output file. If you want to keep a file, you have to rename it. 
+The PNG file will be replaced everytime you start the script with the same output file. 
+If you want to keep a file, you have to rename it. 
 '''
 
 import sys                              #sys files processing
